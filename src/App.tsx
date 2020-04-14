2414dcb750017db3b506ac8289fdc672e09806da
deltafixer <gospodarweba@gmail.com>
2020-04-14 17:21:35 +0200

    feat(stage 2): implement fetching github user repositories

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { IGitHubRepo } from "./models/IGitHubRepo";
+import "./App.scss";
+
+const gitHubUserURL = "https://api.github.com/users/facebook/repos";
+const fetchOptions = {
+  method: "GET",
+  headers: {
+    "Content-Type": "application/json",
+  },
+};
+
+const debounce = (func: any, wait: number, isImmediate = false) => {
+  let timeout: number | NodeJS.Timeout | null;
+  return (...args: any) => {
+    const later = () => {
+      timeout = null;
+      if (!isImmediate) {
+        func(...args);
+      }
+    };
+    const callNow = isImmediate && !timeout;
+    clearTimeout(timeout as number);
+    (timeout as NodeJS.Timeout) = setTimeout(later, wait);
+    if (callNow) {
+      func(...args);
+    }
+  };
+};
+
+const fetchMyRepos = async (): Promise<IGitHubRepo[]> => {
+  const res = await fetch(gitHubUserURL, fetchOptions);
+  return res.json();
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [repos, setRepos] = useState([] as IGitHubRepo[]);
+  const [loading, setLoading] = useState(true);
+  const [searchString, setSearchString] = useState("");
+  let counter = 0;
+
+  useEffect(() => {
+    fetchMyRepos().then((fetchedRepos) => {
+      setRepos(fetchedRepos);
+      setLoading(false);
+    });
+  }, []);
+
+  const searchInitiated = debounce((value: string) => {
+    counter = 0;
+    setSearchString(value);
+  }, 400);
+
+  return loading ? (
+    <div className="loading">Loading repositories...</div>
+  ) : (
+    <div className="app-container">
+      <input
+        type="text"
+        autoFocus
+        placeholder="Type here to search for repos..."
+        onChange={(e) => searchInitiated(e.target.value)}
+      />
+      <div className="repos-container">
+        {repos.map((repo, index) => {
+          if (repo.name.toLowerCase().includes(searchString.toLowerCase())) {
+            counter += 1;
+            return <div key={index}>{repo.name}</div>;
+          }
+          return null;
+        })}
+      </div>
+      {/* kinda using the advantage of renderer, if this div is placed above, using counter like this won't be possible */}
+      <div className="number-of-results">Number of results:&nbsp;{counter}</div>
     </div>
   );
 }
---
+++ src/models/IGitHubRepo.ts
@@ -0,0 +1,4 @@
+export interface IGitHubRepo {
+  // has a lot of fields, I don't need all of them
+  name: string;
+}
